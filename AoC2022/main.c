c385788164aa553778451f324f9c1ba831287196
Anders Runesson <anders@runesson.info>
2022-12-23 14:36:58 +0100

    Initial Commit

---
+++ AoC2022/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AoC2022
+//
+//  Created by Anders Runesson on 2022-12-23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
